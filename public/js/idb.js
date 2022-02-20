0de68c066a5b78dcad075611380139a5507ebd74
Ronya <klmirg6@gmail.com>
2022-02-20 16:23:33 -0600

    working on the indexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -35,4 +35,32 @@ function uploadBudget() {
 
   const getAll = budgetObjectStore.getAll();
 
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(serverResponse => {
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+        const transaction = db.transaction(['new_budget'], 'readwrite');
+        const budgetObjectStore = transaction.objectStore('new_budget');
+        budgetObjectStore.clear();
+
+        alert('All new budgets have been submitted!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  };
 }
+
+window.addEventListener('online', uploadBudget);
