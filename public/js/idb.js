c24e839bf04735e9c3350cb166486f029f167eff
Ronya <klmirg6@gmail.com>
2022-02-20 18:54:45 -0600

    figured out getting the indexedDB workinggit add .

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,10 +1,17 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+  
 let db;
 
 const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  db.createObjectStore('new_budget', { autoIncrememnt: true });
+  db.createObjectStore('new_budget', { autoIncrement: true });
 }
 
 request.onsuccess = function (event) {
