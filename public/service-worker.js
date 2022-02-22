bad8dc10331b70618cb4373e91e66b4c2ff5a8da
Ronya <klmirg6@gmail.com>
2022-02-21 18:27:35 -0600

    everything is working! final touches

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,6 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-
 const FILES_TO_CACHE = [
   './index.html',
   './manifest.json',
@@ -62,76 +61,3 @@ self.addEventListener('fetch', function (e) {
     })
   )
 })
-
-
-
-
-
-
-
-
-// self.addEventListener('install', function(evt) {
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       console.log('Your files were pre-cached successfully!');
-//       return cache.addAll(FILES_TO_CACHE);
-//     })
-//   );
-
-//   self.skipWaiting();
-// });
-
-// self.addEventListener('activate', function(evt) {
-//   evt.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log('Removing old cache data', key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   )
-// });
-
-
-// self.addEventListener('fetch', function(evt) {
-//   if (evt.request.url.includes('/api/')) {
-//     evt.respondWith(
-//       caches
-//         .open(DATA_CACHE_NAME)
-//         .then(cache => {
-//           return fetch(evt.request)
-//           .then(response => {
-//             if (response.status === 200) {
-//               cache.put(evt.request.url, 
-//                 response.clone());
-//             }
-
-//             return response;
-//           })
-//           .catch(err => {
-//             return cache.match(evt.request);
-//           });
-//         })
-//         .catch(err => console.log(err))
-//     );
-
-//     return;
-//   }
-//   evt.respondWith(
-//     fetch(evt.request).catch(function() {
-//       return caches.match(evt.request).then(function(response) {
-//         if (response) {
-//           return response;
-//         } else if (evt.request.headers.get('accept').includes('text/html')) {
-//           // return the cached home page for all requests for html pages
-//           return caches.match('/');
-//         }
-//       });
-//     })
-//   );
-// });
-
